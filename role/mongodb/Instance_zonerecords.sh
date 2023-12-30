6d2e8cc4bac4bb8920859ea12f5f45107f46a376
SUDHAKARREDDYPONUGOTI <ponugotisudhakar468@gmail.com>
2023-12-30 13:38:41 +0530

    IAMinstanceCreation

--- role/mongodb/Instance_zonerecords.sh
+++ role/mongodb/Instance_zonerecords.sh
@@ -16,7 +16,29 @@ do
     else
         $INSTANCE_TYPE=t3.micro
     fi
+
+   IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     
-    aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID
+    
+    echo "$i: $IP_ADDRESS"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $HOSTER_ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    }
+        '
 done 
 
